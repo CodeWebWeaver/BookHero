f717daf2645c4a1f5e395ad24215a9f3642de6df
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-09-05 16:42:52 +0300

    improved repository using jakarta jpa v2.0

--- src/main/java/com/parkhomovsky/bookstore/repository/implementation/BookRepositoryImpl.java
+++ src/main/java/com/parkhomovsky/bookstore/repository/implementation/BookRepositoryImpl.java
@@ -2,32 +2,32 @@ package com.parkhomovsky.bookstore.repository.implementation;
 
 import com.parkhomovsky.bookstore.model.Book;
 import com.parkhomovsky.bookstore.repository.BookRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
 import java.util.List;
 import java.util.Optional;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
+    private final EntityManagerFactory entityManagerFactory;
 
     @Autowired
-    public BookRepositoryImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
+    public BookRepositoryImpl(EntityManagerFactory entityManagerFactory) {
+        this.entityManagerFactory = entityManagerFactory;
     }
 
     @Override
     public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
+        EntityManager entityManager = null;
+        EntityTransaction transaction = null;
         try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.persist(book);
+            entityManager = entityManagerFactory.createEntityManager();
+            transaction = entityManager.getTransaction();
+            entityManager.persist(book);
             transaction.commit();
             return book;
         } catch (Exception e) {
@@ -36,16 +36,16 @@ public class BookRepositoryImpl implements BookRepository {
             }
             throw new RuntimeException("Can`t insert book in database: " + book, e);
         } finally {
-            if (session != null) {
-                session.close();
+            if (entityManager != null) {
+                entityManager.close();
             }
         }
     }
 
     @Override
     public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            Query<Book> allBooksQuery = session.createQuery("from Book", Book.class);
+        try (EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+            TypedQuery<Book> allBooksQuery = entityManager.createQuery("from Book", Book.class);
             return allBooksQuery.getResultList();
         } catch (Exception e) {
             throw new RuntimeException("Failed during gathering all books", e);
@@ -54,13 +54,13 @@ public class BookRepositoryImpl implements BookRepository {
 
     @Override
     public Optional<Book> findById(Long id) {
-        Session session = null;
+        EntityManager entityManager = null;
         try {
-            session = sessionFactory.openSession();
-            return Optional.ofNullable(session.find(Book.class, id));
+            entityManager = entityManagerFactory.createEntityManager();
+            return Optional.ofNullable(entityManager.find(Book.class, id));
         } catch (Exception e) {
-            if (session != null) {
-                session.close();
+            if (entityManager != null) {
+                entityManager.close();
             }
             throw new RuntimeException("Error due searching book with id: " + id);
         }
