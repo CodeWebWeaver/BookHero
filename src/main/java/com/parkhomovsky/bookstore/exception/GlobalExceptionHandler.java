7792aa32a19bb41e99d1ace664eb82b1b7a83f43
Alexander <aleksandrparkhomovsky@gmail.com>
2023-09-11 09:26:29 +0300

    changed exception import at GlobalExceptionHandler v6.0

--- src/main/java/com/parkhomovsky/bookstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/parkhomovsky/bookstore/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.parkhomovsky.bookstore.exception;
 
-import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -13,6 +12,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
