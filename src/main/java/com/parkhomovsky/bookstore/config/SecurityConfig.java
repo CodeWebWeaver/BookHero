8d06a9f4b5aacfd0f9e187b5d8e37b1d49c26fd7
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-09-12 18:12:57 +0300

    fix SecurityConfig removed .httpBasic(withDefaults())

--- src/main/java/com/parkhomovsky/bookstore/config/SecurityConfig.java
+++ src/main/java/com/parkhomovsky/bookstore/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.parkhomovsky.bookstore.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 import com.parkhomovsky.bookstore.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -42,7 +40,6 @@ public class SecurityConfig {
                                 .anyRequest()
                                 .authenticated()
                 )
-                .httpBasic(withDefaults())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter,
