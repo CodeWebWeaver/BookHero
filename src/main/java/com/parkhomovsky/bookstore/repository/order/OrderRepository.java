39c8fe788025b1b00250699de68ad33a514314af
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-09-22 10:02:51 +0300

    fix after review: include cartItem initialization for ShoppingCart v10.3

--- src/main/java/com/parkhomovsky/bookstore/repository/order/OrderRepository.java
+++ src/main/java/com/parkhomovsky/bookstore/repository/order/OrderRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    @Query("SELECT o FROM Order o WHERE o.user = :user")
-    List<Order> findAllByUser(User user);
 
     @Query("SELECT o FROM Order o LEFT JOIN FETCH o.orderItems WHERE o.user = :user")
     List<Order> findAllByUserWithItems(User user);
