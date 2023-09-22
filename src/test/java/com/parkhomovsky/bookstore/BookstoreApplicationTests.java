653726b0f610b891e68dc0cf276e2f979f65f8df
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-09-22 22:33:40 +0300

    docker included v11.0

--- src/test/java/com/parkhomovsky/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/parkhomovsky/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,10 @@
 package com.parkhomovsky.bookstore;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookstoreApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
