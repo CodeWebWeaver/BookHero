c0c433e694784605e8d6d6ec792ac5c5f7bc5766
Alexander Parkhomovsky <69048440+Skarbey@users.noreply.github.com>
2023-09-27 10:30:38 +0300

    some fixes after review v12.1

--- src/test/java/com/parkhomovsky/bookstore/controller/BookControllerTest.java
+++ src/test/java/com/parkhomovsky/bookstore/controller/BookControllerTest.java
@@ -8,7 +8,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.parkhomovsky.bookstore.config.ContextInitializer;
 import com.parkhomovsky.bookstore.dto.book.BookDto;
 import com.parkhomovsky.bookstore.dto.book.CreateBookRequestDto;
 import java.math.BigDecimal;
@@ -23,7 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -31,9 +29,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ContextConfiguration(
-        initializers = ContextInitializer.class
-)
 class BookControllerTest {
     protected static MockMvc mockMvc;
     private static final int VALID_BOOK_ID = 1;
